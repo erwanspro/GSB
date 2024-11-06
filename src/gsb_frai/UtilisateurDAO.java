c228f8db2f5ee69f9b456211bdf14deb5047f547
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-06 11:34:28 +0100

    add lib + modif user et DAO

--- src/gsb_frai/UtilisateurDAO.java
+++ src/gsb_frai/UtilisateurDAO.java
@@ -4,10 +4,44 @@
  */
 package gsb_frai;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author esellin
  */
 public class UtilisateurDAO {
+    
+    private Connection connexion;
+
+    public UtilisateurDAO(Connection connexion) {
+        this.connexion = connexion;
+    }
+    public ArrayList<Utilisateur> getLesUtilisateurs()
+    {
+        ResultSet result = null;
+        ArrayList<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
+        try {
+            String sql = "SELECT * FROM visiteur";
+            Statement statement = connexion.createStatement();
+            result = statement.executeQuery(sql);
+            while(result.next())
+            {
+                Utilisateur user = new Utilisateur(result.getString(4),result.getString(5));
+                lesUtilisateurs.add(user);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccesBdD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lesUtilisateurs;
+         
+         
+    } 
     
 }
