08ad3f5938025279070921148eee0aa714e3fc22
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-06 11:36:49 +0100

    finish users

--- src/gsb_frai/Utilisateur.java
+++ src/gsb_frai/Utilisateur.java
@@ -16,4 +16,26 @@ public class Utilisateur {
         this.login = login;
         this.password = password;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" + "login=" + login + ", password=" + password + '}';
+    }
+    
 }
