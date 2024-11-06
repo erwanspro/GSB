d9103741f49fcf971584cb4a6cb6b65601233927
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-06 10:53:09 +0100

    renommage nom de classe (convention)

--- src/gsb_frai/FenetreApplication.java
+++ src/gsb_frai/FenetreApplication.java
@@ -8,12 +8,12 @@ package gsb_frai;
  *
  * @author cbgam
  */
-public class fenetre_application extends javax.swing.JFrame {
+public class FenetreApplication extends javax.swing.JFrame {
 
     /**
      * Creates new form fenetre_recherche
      */
-    public fenetre_application() {
+    public FenetreApplication() {
         initComponents();
     }
 
@@ -182,21 +182,23 @@ public class fenetre_application extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(fenetre_application.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FenetreApplication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(fenetre_application.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FenetreApplication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(fenetre_application.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FenetreApplication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(fenetre_application.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FenetreApplication.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new fenetre_application().setVisible(true);
+                new FenetreApplication().setVisible(true);
             }
         });
     }
