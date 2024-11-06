5d382360e08640eb5e952ce4d0c21ff326a6f4fd
esellin <esellin@B4-WIN11-126.290070u.local>
2024-11-06 11:15:01 +0100

    add accesbdd

---
+++ src/gsb_frai/AccesBdD.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gsb_frai;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author esellin
+ */
+public class AccesBdD {
+    
+    private final String dbURL="jdbc:mysql://localhost:3306/bibliotheque";
+    private final String userName = "root";
+    private final String password = "";
+    private Connection connexion;
+    
+    public AccesBdD()
+    {
+        try {
+            this.connexion = DriverManager.getConnection(dbURL,userName,password);
+        } catch (SQLException ex) {
+            Logger.getLogger(AccesBdD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Connection getConnexion()
+    {
+        return this.connexion;
+    }
+}
